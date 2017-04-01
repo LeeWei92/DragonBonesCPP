cbe9de448241b1dfe9a12009bb465055964d4416
WeiLee <shengxiaainj@gmail.com>
2017-04-01 16:55:57 +0800

    Update Android.mk

--- dragonBones/dragonBones/Android.mk
+++ dragonBones/dragonBones/Android.mk
@@ -11,7 +11,6 @@ endef
 #遍历LOCAL_PATH目录
 ALLFILES = $(call walk, $(LOCAL_PATH))
 
-# FILE_LIST := hellocpp/main.cpp
 #从所有文件中提取出所有的.cpp和.c文件
 FILE_LIST += $(filter %.cpp %.c, $(ALLFILES))
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
